755b82c475a9ce714202bee80b3bc04d8dd20baa
Dominik <dominik7550j@gmail.com>
2023-03-08 19:26:05 +0100

    Intial GET, POST, PUT, DELETE"
    "

--- backend/contollers/goalContollers.js
+++ backend/contollers/goalContollers.js
@@ -1,12 +1,14 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalsModel')
+
 
 // route: GET /api/goals
 const getGoals = asyncHandler(async (request, response) => {
-    response.status(200).json(
-        {
-            message: 'Get goals from rotuer'
-        })
+    const goals = await Goal.find()
+    
+    response.status(200).json(goals)
 })
+ 
 
 // route: POST /api/goals
 const setGoals = asyncHandler(async (request, response) => {
@@ -14,27 +16,42 @@ const setGoals = asyncHandler(async (request, response) => {
         response.status(400)
         throw new Error('Please add a text field')
     }
-    response.status(200).json(
-        {
-            message: 'Post goals from rotuer'
-        })
+
+    const goal = await Goal.create({
+        text: request.body.text,
+
+    })
+
+    response.status(200).json(goal)
 })
 
 // route: PUT /api/goals/:id
 const putGoals = asyncHandler(async (request, response) => {
-    response.status(200).json(
-        {
-            message: 'Put goals from rotuer. Id: ' + request.params.id
-        })
+    const goal = await Goal.findById(request.params.id);
+    if (!goal ) {
+        response.status(400)
+        throw new Error("Goal not found")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(request.params.id, request.body, {
+            new: true,
+        }
+    )
+    response.status(200).json(updatedGoal)
 })
 
 // route: DELETE /api/goals/:id
 const deleteGoals = asyncHandler(async (request, response) => {
-    response.status(200).json(
-        {
-            message: 'Delete goals from rotuer. Id: ' + request.params.id
-        })
+    const goal = await Goal.findById(request.params.id);
+    if (!goal ) {
+        response.status(400)
+        throw new Error("Goal not found")
+    }
+
+    const removedGoal = await goal.remove()
+    response.status(200).json(removedGoal)
 })
+
 
 module.exports = {
     getGoals,
