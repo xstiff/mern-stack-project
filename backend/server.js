1b7eadf64b83458110f599b77bb147cbf524568e
Dominik <dominik7550j@gmail.com>
2023-03-22 20:00:27 +0100

    Prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 console.clear()
 
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -17,6 +18,16 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./rotues/goalRoutes'))
 app.use('/api/users', require('./rotues/userRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
+else {
+    app.get('/', (req, res) => res.send('Please set mode to production'))
+}
 
 app.use(errorHandler)
 app.listen(port, () => console.log("Server is running on ", port))
